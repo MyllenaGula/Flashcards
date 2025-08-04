6fe73281149fef8374232efeef6941dbb2c490b0
Myllena Tratsch Gula <gula.myllena@escola.pr.gov.br>
2025-08-04 12:02:42 -0300

    v

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,21 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Inteligência Artificial',
+    'O que é Inteligência Artificial?',
+    'IA é a capacidade de uma máquina de exibir inteligência, como aprender, resolver problemas e tomar decisões, geralmente simulando habilidades humanas. '
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Inteligência Artificial',
+    'Onde a IA está sendo usada atualmente??',
+    'A IA já está presente em diversas áreas, como assistentes virtuais (Siri, Alexa), carros autônomos, sistemas de recomendação, análise de dados, diagnóstico médico e detecção de fraudes. '
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Inteligência Artificial',
+    'A IA pode ser tendenciosa?',
+    'Sim, os algoritmos de IA podem refletir os vieses presentes nos dados com os quais foram treinados, o que pode levar a resultados injustos ou discriminatórios. '
 )
     criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Inteligência Artificial',
+    'A possibilidade de a IA alcançar e superar a inteligência humana é um tópico de pesquisa e debate em andamento, com visões divergentes sobre o futuro da IA. '
 )
